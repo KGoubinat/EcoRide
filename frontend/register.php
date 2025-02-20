acb3a946aab7af0e1586da246cf9645648a750d4
Goub <kevingoub@gmail.com>
2025-02-20 10:31:16 +0100

    correction path

--- frontend/register.php
+++ frontend/register.php
@@ -2,7 +2,7 @@
 session_start();
 header("Content-Type: application/json");
 
-require_once '/cloudinary_php_master/src/Cloudinary.php';
+require_once '/../cloudinary_php_master/src/Cloudinary.php';
 
 \Cloudinary::config(array(
     "cloud_name" => "dj9iiquhw",
