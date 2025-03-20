baf585e261927fbe366a343e7dd2c817f8156c11
Goub <kevingoub@gmail.com>
2025-03-20 09:26:15 +0100

    c

--- frontend/js/profil.js
+++ frontend/js/profil.js
@@ -17,19 +17,34 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
     
-    const authButton = document.getElementById('authButton');
-    const profilButton = document.getElementById('profilButton');
-
-    const isLoggedIn = authButton.getAttribute('data-logged-in') === 'true';
-
-    console.log("Is user logged in? " + isLoggedIn); // Affiche dans la console si l'utilisateur est connecté ou non
-
-    if (isLoggedIn) {
-        authButton.innerHTML = '<a href="/frontend/deconnexion.php">Déconnexion</a>';
-        profilButton.innerHTML = '<a href="/frontend/profil.php">Profil</a>';
-    } else {
-        authButton.innerHTML = '<a href="/frontend/connexion.html">Connexion</a>';
-        profilButton.style.display = 'none';  // Masquer le bouton Profil
-    }
+     // Gestion de l'authentification utilisateur
+     const authButton = document.getElementById('authButton');
+     const profilButton = document.getElementById('profilButton');
+     const authButtonMobile = document.getElementById('authButtonMobile');
+     const profilButtonMobile = document.getElementById('profilButtonMobile');
+ 
+     if (authButton && profilButton) {
+         const isLoggedIn = authButton.getAttribute('data-logged-in') === 'true';
+ 
+         console.log("Is user logged in? " + isLoggedIn); // Debug console
+ 
+         if (isLoggedIn) {
+             authButton.innerHTML = '<a href="/frontend/deconnexion.php">Déconnexion</a>';
+             profilButton.innerHTML = '<a href="/frontend/profil.php">Profil</a>';
+ 
+             if (authButtonMobile && profilButtonMobile) {
+                 authButtonMobile.innerHTML = '<a href="/frontend/deconnexion.php">Déconnexion</a>';
+                 profilButtonMobile.innerHTML = '<a href="/frontend/profil.php">Profil</a>';
+             }
+         } else {
+             authButton.innerHTML = '<a href="/frontend/connexion.html">Connexion</a>';
+             profilButton.style.display = 'none';  // Masquer le bouton Profil
+ 
+             if (authButtonMobile && profilButtonMobile) {
+                 authButtonMobile.innerHTML = '<a href="/frontend/connexion.html">Connexion</a>';
+                 profilButtonMobile.style.display = 'none';  // Masquer le bouton Profil
+             }
+         }
+     }
 
 })
