dffb8802ba21bbad10a2e5a87a0967b342bf6348
Goub <kevingoub@gmail.com>
2025-04-17 10:09:34 +0200

    c

--- frontend/details.php
+++ frontend/details.php
@@ -1,4 +1,11 @@
 <?php
+if (empty($_SERVER['HTTPS']) && ($_SERVER['HTTP_X_FORWARDED_PROTO'] ?? '') !== 'https') {
+    $httpsUrl = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header("Location: $httpsUrl", true, 301);
+    exit();
+}
+
+
 session_start();
 
 // Régénérer l'ID de session à chaque nouvelle connexion pour éviter la fixation de session
